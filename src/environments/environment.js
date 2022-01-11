b418a5e29b6484c881705d521ef46a904229f462
me <me@mes-MacBook-Pro.local>
2022-01-11 15:25:02 -0500

    stuff

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,4 +1,6 @@
-
+/**
+ * For a list of all our constants & env files, see constants.js
+ */
 export const environment = {
 
     // backendApi: 'http://localhost:5001/nft-discord-relay/us-central1/api'  // firebase functions emulator - only needs local when you make changes to the API, and you need to test
