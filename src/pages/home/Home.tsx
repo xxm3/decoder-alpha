97768a585f251f02984913c0879ed11685935d21
Mike <napindc@vt.edu>
2022-01-20 17:13:44 -0500

    hnghh

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,6 @@
-import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRouterLink} from '@ionic/react';
-import {pin} from 'ionicons/icons';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRouterLink} from '@ionic/react';
+import { IonItem, IonLabel, IonCard, IonCardContent, IonIcon, IonRow, IonCol } from '@ionic/react';
 import React, {useEffect, useState} from 'react';
-import {
-    IonItem,
-    IonLabel,
-    IonCard,
-    IonCardContent,
-    IonIcon,
-    IonRow,
-    IonCol,
-} from '@ionic/react';
 import axios from 'axios';
 import './Home.css';
 import HeaderContainer from "../../components/header/HeaderContainer";
@@ -17,19 +8,44 @@ import Card from './Card';
 import CollectionCard from './CollectionCard';
 import Loader from "../../components/search/Loader";
 import {environment} from "../../environments/environment";
-import {setTimeout} from "timers";
 import {
     resolveToWalletAddress,
     getParsedNftAccountsByOwner,
 } from "@nfteyez/sol-rayz";
-import {Connection, programs} from '@metaplex/js';
+
+// import { PublicKey } from '@solana/web3.js';
+// import { useWallet } from '@solana/wallet-adapter-react';
+// import * as anchor from '@project-serum/anchor';
+// import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
+// import { SystemProgram } from '@solana/web3.js';
+// import {
+//     LedgerWalletAdapter,
+//     PhantomWalletAdapter,
+//     SlopeWalletAdapter,
+//     SolflareWalletAdapter,
+//     SolletExtensionWalletAdapter,
+//     SolletWalletAdapter,
+//     TorusWalletAdapter,
+// } from '@solana/wallet-adapter-wallets';
+// import {
+//     WalletModalProvider,
+//     WalletDisconnectButton,
+//     WalletMultiButton
+// } from '@solana/wallet-adapter-react-ui';
+// import { clusterApiUrl } from '@solana/web3.js';
+// import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
+// import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+
+// Default styles that can be overridden by your app
+// require('@solana/wallet-adapter-react-ui/styles.css');
+
 
 const Home = () => {
 
     /**
      * State Variables
      */
-        // TODO-vinit: need this error fixed - https://sentry.io/answers/unique-key-prop/
+    // TODO-vinit: need this error fixed - https://sentry.io/answers/unique-key-prop/
     const [walletAddress, setWalletAddress] = useState('');
     const [products, setProducts] = useState([]);
     const [newcollections, setNewCollection] = useState([]);
@@ -41,93 +57,99 @@ const Home = () => {
     /**
      * Actions
      */
-        // called from the child, after their wallet is connected
+    // called from the child, after their wallet is connected
     const mintAddrToParent = (walletAddress: any) => {
-            // console.log(`----got wallet address from child: '${walletAddress}'`);
-            setWalletAddress(walletAddress);
-            getNfts(walletAddress);
-        }
+        // console.log(`----got wallet address from child: '${walletAddress}'`);
+        setWalletAddress(walletAddress);
+        getNfts(walletAddress);
+    }
 
     /**
      * UseEffects
      */
 
-        // gets the user's nft's from their wallet
-        // from https://github.com/NftEyez/sol-rayz
+    // gets the user's nft's from their wallet
+    // from https://github.com/NftEyez/sol-rayz
     const getNfts = async (passedWalletAddress: string) => {
+        const publicAddress = passedWalletAddress;
 
-            const publicAddress = passedWalletAddress;
+        const rawNftArray = await getParsedNftAccountsByOwner({
+            publicAddress,
+        });
 
-            const rawNftArray = await getParsedNftAccountsByOwner({
-                publicAddress,
-            });
+        // console.log("raw user nfts: ", rawNftArray);
 
-            // console.log("raw user nfts: ", rawNftArray);
+        let modifiedUserNfts: any = [];
 
-            let modifiedUserNfts: any = [];
+        for (let i in rawNftArray) {
+            const uri = rawNftArray[i].data.uri;
 
-            for (let i in rawNftArray) {
-                const uri = rawNftArray[i].data.uri;
+            if (uri.indexOf("arweave") !== -1) {
+                let moreData: any = {};
 
-                if (uri.indexOf("arweave") !== -1) {
-                    let moreData: any = {};
-
-                    await axios.get(uri).then((res) => {
-                        // push unique collections only
-                        // @ts-ignore
-                        if (!modifiedUserNfts.map(item => item.name).includes(res.data.collection.name)) {
-                            modifiedUserNfts.push({
-                                img: res.data.image,
-                                name: res.data.collection.name
-                            });
-                        }
-                    })
-                        .catch((err) => {
-                            console.error("error when getting arweave data: " + err);
+                await axios.get(uri).then((res) => {
+                    // push unique collections only
+                    // @ts-ignore
+                    if (!modifiedUserNfts.map(item => item.name).includes(res.data.collection.name)) {
+                        modifiedUserNfts.push({
+                            img: res.data.image,
+                            name: res.data.collection.name
                         });
-                }
+                    }
+                })
+                    .catch((err) => {
+                        console.error("error when getting arweave data: " + err);
+                    });
             }
-
-            // console.log("modified user nfts: ", modifiedUserNfts);
-
-            // @ts-ignore
-            setUserNfts(modifiedUserNfts);
         }
 
+        // console.log("modified user nfts: ", modifiedUserNfts);
 
-    const getCmidDetails = async () => {
-        // const { metadata: { Metadata } } = programs;
-        const {metaplex: {Store, AuctionManager}, metadata: {Metadata}, auction: {Auction}, vault: {Vault}} = programs;
-
-
-        // const connection = new Connection('mainnet-beta');
-        // const tokenPublicKey = '9udKMALG9vYXvwdQK6CUfXdsn4SiWwWtzTyMpzLF7g41';
-
-        // try {
-        //     const ownedMetadata = await Metadata.load(connection, tokenPublicKey);
-        //     console.log(ownedMetadata);
-        // } catch(err) {
-        //     console.error('Failed to fetch metadata');
-        //     console.error(err);
-        // }
-
-        // const connection = new Connection('devnet');
-        // const tokenPublicKey = 'Gz3vYbpsB2agTsAwedtvtTkQ1CG9vsioqLW3r9ecNpvZ';
-        //
-        // const metadata = await Metadata.load(connection, tokenPublicKey);
-        // const auction = await Auction.load(connection, tokenPublicKey);
-        // const vault = await Vault.load(connection, tokenPublicKey);
-        // const auctionManager = await AuctionManager.load(connection, tokenPublicKey);
-        // const store = await Store.load(connection, tokenPublicKey);
-        // console.log(metadata);
-        // console.log(auction);
-        // console.log(vault);
-        // console.log(auctionManager);
-        // console.log(store);
+        // @ts-ignore
+        setUserNfts(modifiedUserNfts);
+    }
 
 
-    };
-    getCmidDetails();
+    // TO.DO
+    // const wallet = useWallet();
+    // const getCmidDetails = async () => {
+    //
+    //     // TO.DO: pass in
+    //     const candyMachineId = "6FKrA68HvuNa5GRqZiGzRrSBXWou8tmv1HT73PBPWwiC"; // relic
+    //     const candyPk = new anchor.web3.PublicKey(candyMachineId);
+    //
+    //     const CANDY_MACHINE_PROGRAM = new anchor.web3.PublicKey(
+    //         'cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ',
+    //     );
+    //
+    //     // TO.DO: ssc
+    //     // const connection = new anchor.web3.Connection(rpcHost ? rpcHost : anchor.web3.clusterApiUrl('devnet'));
+    //     const connection = new anchor.web3.Connection(anchor.web3.clusterApiUrl('mainnet-beta'));
+    //
+    //     // const anchorWallet = {
+    //     //     publicKey: wallet.publicKey,
+    //     //     signAllTransactions: wallet.signAllTransactions,
+    //     //     signTransaction: wallet.signTransaction,
+    //     // }
+    //     //
+    //     // // TO.DO: no red
+    //     // const provider = new anchor.Provider(connection, anchorWallet, {
+    //     //     preflightCommitment: 'recent',
+    //     // });
+    //     //
+    //     // const idl = await anchor.Program.fetchIdl(CANDY_MACHINE_PROGRAM, provider);
+    //     // const program = new anchor.Program(idl, CANDY_MACHINE_PROGRAM, provider);
+    //     //
+    //     // const state: any = await program.account.candyMachine.fetch(candyPk);
+    //     // const itemsAvailable = state.data.itemsAvailable.toNumber();
+    //     // const itemsRedeemed = state.itemsRedeemed.toNumber();
+    //     // const itemsRemaining = itemsAvailable - itemsRedeemed;
+    //     //
+    //     // console.log(state);
+    // };
+    // getCmidDetails();
+
+
 
 
     /**
@@ -159,12 +181,37 @@ const Home = () => {
             });
     };
 
+
+    // // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    // const network = WalletAdapterNetwork.Devnet;
+    //
+    // // You can also provide a custom RPC endpoint.
+    // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    //
+    // // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // // of wallets that your users connect to will be loaded.
+    // const wallets = useMemo(
+    //     () => [
+    //         new PhantomWalletAdapter(),
+    //         new SlopeWalletAdapter(),
+    //         new SolflareWalletAdapter(),
+    //         new TorusWalletAdapter(),
+    //         new LedgerWalletAdapter(),
+    //         new SolletWalletAdapter({ network }),
+    //         new SolletExtensionWalletAdapter({ network }),
+    //     ],
+    //     [network]
+    // );
+
+
     // @ts-ignore
     return (
         <IonPage className="bg-sky">
 
             <HeaderContainer mintAddrToParent={mintAddrToParent} showflag={true} onClick={undefined}/>
             <IonContent>
+
                 <IonRow>
                     <IonLabel className="text-7xl text-blue-600">New Collection</IonLabel>
                 </IonRow>
@@ -263,6 +310,17 @@ const Home = () => {
                     </IonRow>
                 </IonContent>
             </IonCard>
+
+            {/*<ConnectionProvider endpoint={endpoint}>*/}
+            {/*    <WalletProvider wallets={wallets} autoConnect>*/}
+            {/*        <WalletModalProvider>*/}
+            {/*            <WalletMultiButton />*/}
+            {/*            <WalletDisconnectButton />*/}
+            {/*            { /* Your app's components go here, nested within the context providers. */ }*/}
+            {/*        </WalletModalProvider>*/}
+            {/*    </WalletProvider>*/}
+            {/*</ConnectionProvider>*/}
+
 
         </IonPage>
     );
