891fb79c556325e457a3ffc10f7453d22ae51c0b
Sparrow <sparrow.tech24@gmail.com>
2022-01-25 22:54:36 +0530

    Remove code formatting changes

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,23 +3,20 @@ import React, { useRef } from "react";
 import Display from "../components/search/Display";
 import { useState, useEffect, useContext } from "react";
 import { Message } from "../data/messages";
-
-import { IonContent, IonPage, IonSearchbar, IonButton } from "@ionic/react";
+import { IonContent, IonPage, IonButton } from "@ionic/react";
 import "./Search.css";
 import faker from "faker";
-import { setTimeout } from "timers";
 import { MessageContext } from "../context/context";
 import MobileDisplay from "../components/search/MobileDisplay";
-import { useParams } from "react-router";
-import HeaderContainer from "../components/header/HeaderContainer";
+import { environment } from "../environments/environment";
+import { useParams, useHistory } from "react-router";
+import Header from "../components/header/Header";
 import { instance } from "../axios";
 
 const Search: React.FC = () => {
 	// @ts-ignore
-
 	const { messages, setMessages, setWord } = useContext(MessageContext);
 	const [total, setTotal] = useState(0);
-	const [showHelp, setShowHelp] = useState(true);
 	const [width, setWidth] = useState(window.innerWidth);
 
 	window.onresize = () => {
@@ -67,34 +64,6 @@ const Search: React.FC = () => {
 		return labels.reverse();
 	};
 	const labels = generateLabels();
-	// const [messages, setMessages] = useState<Message[]>([]);
-
-	const [doughnutData, setDoughnutData] = useState({
-		labels: labels,
-		datasets: [
-			// {
-			//     label: '# of Votes',
-			//     data: [12, 19, 3, 5, 2, 3],
-			//     backgroundColor: [
-			//         'rgba(255, 99, 132, 0.8)',
-			//         'rgba(54, 162, 235, 0.8)',
-			//         'rgba(255, 206, 86, 0.8)',
-			//         'rgba(75, 192, 192, 0.8)',
-			//         'rgba(153, 102, 255, 0.8)',
-			//         'rgba(255, 159, 64, 0.8)',
-			//     ],
-			//     borderColor: [
-			//         'rgba(255, 99, 132, 1)',
-			//         'rgba(54, 162, 235, 1)',
-			//         'rgba(255, 206, 86, 1)',
-			//         'rgba(75, 192, 192, 1)',
-			//         'rgba(153, 102, 255, 1)',
-			//         'rgba(255, 159, 64, 1)',
-			//     ],
-			//     borderWidth: 2,
-			// },
-		],
-	});
 
 	const [chartData, setChartData] = useState({
 		labels: dispLabels(),
@@ -131,9 +100,6 @@ const Search: React.FC = () => {
 		try {
 			setIsLoading(true);
 
-			// testing CORS stuff...
-			// await fetch(environment.backendApi + '/receiver/urlParser?url=https://google.com/', { method: 'GET' });
-
 			const { data: fetchedData } = await instance.post(
 				"/search/",
 				{
@@ -148,40 +114,6 @@ const Search: React.FC = () => {
 
 			let sample = fetchedData;
 			setTotal(sample.totalCount);
-
-			setDoughnutData({
-				...doughnutData,
-				// labels: sample.ten_day_count.map(x => x.date),
-				// datasets: [
-				//     {
-				//         label: '10 days count',
-				//         data: sample.ten_day_count.map(x => x.count),
-				//         backgroundColor: [
-				//             'rgba(255, 99, 132, 0.8)',
-				//             'rgba(54, 162, 235, 0.8)',
-				//             'rgba(255, 206, 86, 0.8)',
-				//             'rgba(75, 192, 192, 0.8)',
-				//             'rgba(153, 102, 255, 0.8)',
-				//             'rgba(255, 159, 64, 0.8)',
-				//         ],
-				//         borderColor: [
-				//             'rgba(255, 99, 132, 1)',
-				//             'rgba(54, 162, 235, 1)',
-				//             'rgba(255, 206, 86, 1)',
-				//             'rgba(75, 192, 192, 1)',
-				//             'rgba(153, 102, 255, 1)',
-				//             'rgba(255, 159, 64, 1)',
-				//         ],
-				//         borderWidth: 2,
-				//     },
-				// ]
-			});
-
-			// const handleKeyDown = (event: any) => {
-			//     if (event.key === 'Enter') {
-			//         onClick(event);
-			//     }
-			// }
 
 			// repeated on constants.js & Search.tsx
 			const numDaysBackGraphs = 10;
@@ -229,7 +161,6 @@ const Search: React.FC = () => {
 
 			// console.log(messages);
 
-			setShowHelp(false);
 			setTimeout(() => {
 				setFoundResults(true);
 			}, 2000);
@@ -271,6 +202,7 @@ const Search: React.FC = () => {
 	useEffect(() => {
 		doSearch();
 	}, [searchText]);
+
 	const [walletAddress, setWalletAddress] = useState("");
 
 	/**
@@ -284,71 +216,30 @@ const Search: React.FC = () => {
 	const scrollToTop = () => {
 		contentRef.current && contentRef.current.scrollToTop();
 	};
-	const childRef = useRef<typeof HeaderContainer>(HeaderContainer);
+
 	return (
 		<React.Fragment>
 			<IonPage id="home-page">
 				<IonContent ref={contentRef} scrollEvents={true} fullscreen>
-					<HeaderContainer
+					{/* Header */}
+					<Header
 						mintAddrToParent={mintAddrToParent}
 						onClick={onClick}
 						showflag={false}
 					/>
+
+					{/* Main Content After Header */}
 					<div className="min-h-screen font-sans bg-gradient-to-b from-bg-primary to-bg-secondary flex justify-center items-center p-4 pt-2">
+						{/* The Gray Container */}
 						<div
 							className={` ${
 								width <= 640 ? "w-full" : "container"
 							} bg-satin-3 rounded-lg pt-3 pb-6 pr-3 pl-3 h-fit xl:pb-3 2xl:pb-2 lg:pb-4`}
 						>
-							{/* search bar / form */}
-							<form onSubmit={(e) => onClick(e)}>
-								{(!foundResults || isLoading) && (
-									<>
-										<h1 className="text-center font-bold text-white text-4xl">
-											{isLoading ? (
-												<p>
-													Searching for{" "}
-													<b className="text-cb">
-														{searchText}
-													</b>
-												</p>
-											) : (
-												"Search:"
-											)}
-										</h1>
-										{/*<p className="mx-auto font-normal text-center text-sm my-6 max-w-lg">This app will last 10 days count and last 100 messages.</p>*/}
-									</>
-								)}
-								{/* bg-cbgd bg-bg-secondary */}
-								<div className="xs:flex items-center rounded-lg overflow-hidden px-2 py-1 justify-between">
-									<IonSearchbar
-										className="xs-flex text-base text-gray-400 flex-grow outline-none px-2 "
-										type="text"
-										value={searchText}
-										onIonChange={(e) => {
-											setSearchText(e.detail.value!);
-										}}
-										animated
-										placeholder="Type to search"
-										disabled={isLoading}
-									/>
-									<div className="xs:flex items-center px-2 rounded-lg space-x-4 mx-auto ">
-										<IonButton
-											className=" text-white text-base rounded-lg"
-											onClick={() => onClick}
-											animate-bounce
-											disabled={searchText === ""}
-										>
-											Search
-										</IonButton>
-									</div>
-								</div>
-							</form>
-
 							{/* loading bar */}
 							{isLoading && (
 								<div className="pt-10 flex justify-center items-center">
-									<Loader></Loader>
+									<Loader />
 								</div>
 							)}
 
@@ -357,15 +248,13 @@ const Search: React.FC = () => {
 							{!isLoading && foundResults && width > 1536 && (
 								<Display
 									chartData={chartData}
-									doughnutData={doughnutData}
 									position="bottom"
 									height={Number(35)}
 									total={total}
 									totalCountHeight={18}
 									showPie={false}
 									width={width}
-								></Display>
-								// height={Number(10 + 65)}   75
+								/>
 							)}
 							{!isLoading &&
 								foundResults &&
@@ -373,15 +262,13 @@ const Search: React.FC = () => {
 								width > 1280 && (
 									<Display
 										chartData={chartData}
-										doughnutData={doughnutData}
 										position="bottom"
 										height={Number(45)}
 										total={total}
 										totalCountHeight={22}
 										showPie={false}
 										width={width}
-									></Display>
-									// height={Number(75 + 10)} 85
+									/>
 								)}
 							{!isLoading &&
 								foundResults &&
@@ -389,15 +276,13 @@ const Search: React.FC = () => {
 								width > 1024 && (
 									<Display
 										chartData={chartData}
-										doughnutData={doughnutData}
 										position="bottom"
 										height={Number(55)}
 										total={total}
 										totalCountHeight={25}
 										showPie={false}
 										width={width}
-									></Display>
-									// height={Number(5 + 100)}     105
+									/>
 								)}
 							{!isLoading &&
 								foundResults &&
@@ -405,15 +290,13 @@ const Search: React.FC = () => {
 								width > 768 && (
 									<Display
 										chartData={chartData}
-										doughnutData={doughnutData}
 										position="bottom"
 										height={Number(65)}
 										total={total}
 										totalCountHeight={28}
 										showPie={false}
 										width={width}
-									></Display>
-									// height={Number(5 + 100)}     105
+									/>
 								)}
 							{!isLoading &&
 								foundResults &&
@@ -421,28 +304,25 @@ const Search: React.FC = () => {
 								width > 640 && (
 									<Display
 										chartData={chartData}
-										doughnutData={doughnutData}
 										position="bottom"
 										height={Number(230)}
 										total={total}
 										totalCountHeight={35}
 										showPie={false}
 										width={width}
-									></Display>
-									// height={Number(5 + 225)}     230
+									/>
 								)}
 							{!isLoading && foundResults && width <= 640 && (
 								<MobileDisplay
 									chartData={chartData}
-									doughnutData={doughnutData}
 									position="right"
 									height={Number(310)}
 									total={total}
 									totalCountHeight={30}
 									showPie={false}
-								></MobileDisplay>
-								// height={Number(30 + 275)}       310      width={width}
+								/>
 							)}
+
 							{/* error bar */}
 							{!isLoading && !foundResults && error !== "" && (
 								<div className="relative mt-6 bg-red-100 p-6 rounded-xl">
@@ -452,14 +332,20 @@ const Search: React.FC = () => {
 									<span className="absolute bg-red-500 w-8 h-8 flex items-center justify-center font-bold text-green-50 rounded-full -top-2 -left-2">
 										!
 									</span>
-									<div className="absolute top-0 right-0 flex space-x-2 p-4"></div>
+									{/*<div className="absolute top-0 right-0 flex space-x-2 p-4"></div>*/}
 								</div>
+							)}
+
+							{!isLoading && foundResults && (
+								<IonButton
+									onClick={() => scrollToTop()}
+									className="float-right"
+								>
+									Scroll to Top
+								</IonButton>
 							)}
 						</div>
 					</div>
-					<IonButton onClick={() => scrollToTop()}>
-						Scroll Top
-					</IonButton>
 				</IonContent>
 			</IonPage>
 		</React.Fragment>
