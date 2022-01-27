ea9648fe0402ae01823930dd0c0a8d9674011c1a
Mike <napindc@vt.edu>
2022-01-26 23:16:43 -0500

    stuff

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -3,8 +3,8 @@
  */
 export const environment = {
 
-    backendApi: "https://us-central1-nft-discord-relay.cloudfunctions.net/api", // when on firebase
-    // backendApi: 'http://localhost:5001/nft-discord-relay/us-central1/api'  // firebase functions emulator - only needs local when you make changes to the API, and you need to test
+    // backendApi: "https://us-central1-nft-discord-relay.cloudfunctions.net/api", // when on firebase
+    backendApi: 'http://localhost:5001/nft-discord-relay/us-central1/api'  // firebase functions emulator - only needs local when you make changes to the API, and you need to test
 
     // npm start - only needs local when you make changes to the API, and you need to test
     // change things between environment.js & app.js
