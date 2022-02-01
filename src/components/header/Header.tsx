e6c92e6bd54f2385f99e8aaa0ff8da1416f85496
Mike <napindc@vt.edu>
2022-02-01 13:28:06 -0500

    unmessed upmabye

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,23 +11,6 @@ import {
 } from "@ionic/react";
 import {useHistory} from 'react-router';
 import { search, closeOutline } from 'ionicons/icons';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-    LedgerWalletAdapter,
-    PhantomWalletAdapter,
-    SlopeWalletAdapter,
-    SolflareWalletAdapter,
-    SolletExtensionWalletAdapter,
-    SolletWalletAdapter,
-    TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
@@ -161,28 +144,6 @@ const HeaderContainer = ({mintAddrToParent, showflag, onClick}) => {
         setShowMobileSearch(!showMobileSearch);
     }
 
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.Devnet;
-
-    // You can also provide a custom RPC endpoint.
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-    // Only the wallets you configure here will be compiled into your application, and only the dependencies
-    // of wallets that your users connect to will be loaded.
-    const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-            new SlopeWalletAdapter(),
-            new SolflareWalletAdapter(),
-            new TorusWalletAdapter(),
-            new LedgerWalletAdapter(),
-            new SolletWalletAdapter({ network }),
-            new SolletExtensionWalletAdapter({ network }),
-        ],
-        [network]
-    );
-
     /**
      * Renders
      */
@@ -249,16 +210,6 @@ const HeaderContainer = ({mintAddrToParent, showflag, onClick}) => {
 
 
                         {/*wallet stuff*/}
-
-                        <ConnectionProvider endpoint={endpoint}>
-                            <WalletProvider wallets={wallets} autoConnect>
-                                <WalletModalProvider>
-                                    <WalletMultiButton />
-                                    <WalletDisconnectButton />
-                                    { /* Your app's components go here, nested within the context providers. */ }
-                                </WalletModalProvider>
-                            </WalletProvider>
-                        </ConnectionProvider>
 
                         {/*{!isWalletConnected && width >= 750 && (*/}
                         {/*    <>*/}
