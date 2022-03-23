fd9009c3fd76ec63923858dfee4db44138d3cdff
Damjan <damjandimitrov1@gmail.com>
2022-03-23 20:04:32 +0100

    Implement alert wallet address add

--- src/pages/Alerts.tsx
+++ src/pages/Alerts.tsx
@@ -1,37 +1,25 @@
-import {ChartData} from "chart.js";
-import React, {useEffect, useMemo, useState} from "react";
-import {instance} from "../axios";
-import {dispLabelsDailyCount, getDailyCountData} from "../util/charts";
-import SearchBar from "../components/SearchBar";
-import Loader from "../components/Loader";
-import {Chart} from "react-chartjs-2";
-import Help from "../components/Help";
+import { useEffect, useState } from "react";
+import { instance } from "../axios";
 import {
     IonButton,
-    IonContent,
-    IonHeader,
-    IonIcon,
     IonInput,
     IonItem,
     IonLabel,
-    IonModal,
-    IonTitle,
-    IonToolbar, useIonToast
+    useIonToast
 } from "@ionic/react";
-import {close} from "ionicons/icons";
-import {environment} from "../environments/environment";
-import {Link, useLocation} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {RootState} from "../redux/store";
+import { environment } from "../environments/environment";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
-function StackedSearch({foo, onSubmit}: any) {
+function StackedSearch({ foo, onSubmit }: any) {
 
     /**
      * States & Variables
      */
     const [present, dismiss] = useIonToast();
-    const [formAddAlertToken, setFormAddAlertToken] = useState('');
-    const [formLoadingAddAlertToken, setFormLoadingAddAlertToken] = useState(false); // form loading
+    const [formAddalertWalletAddress, setFormAddalertWalletAddress] = useState('');
+    const [formLoadingAddalertWalletAddress, setFormLoadingAddalertWalletAddress] = useState(false); // form loading
 
     const walletAddress = useSelector(
         (state: RootState) => state.wallet.walletAddress
@@ -46,7 +34,7 @@ function StackedSearch({foo, onSubmit}: any) {
     // fill in their wallet from 'connect wallet', if set...
     useEffect(() => {
         // @ts-ignore
-        setFormAddAlertToken(walletAddress);
+        setFormAddalertWalletAddress(walletAddress);
     }, [walletAddress]);
 
     /**
@@ -60,7 +48,7 @@ function StackedSearch({foo, onSubmit}: any) {
     // in the form for multiple wallets - submit button clicked
     const addAlertsTokenSubmit = (enable: boolean) => {
 
-        if (!formAddAlertToken || formAddAlertToken.length !== 44) {
+        if (!formAddalertWalletAddress || formAddalertWalletAddress.length !== 44) {
             present({
                 message: 'Error - please enter a single, valid SOL wallet address',
                 color: 'danger',
@@ -69,66 +57,46 @@ function StackedSearch({foo, onSubmit}: any) {
             return;
         }
 
-        setFormLoadingAddAlertToken(true);
+        setFormLoadingAddalertWalletAddress(true);
 
-        try {
-            setFormLoadingAddAlertToken(false); // loading false
-            setFormAddAlertToken(''); // clear the form
-            // setMultWalletAryFromCookie(cookies.get('multWalletsAry')); // set array to show user on frontend
+        setFormAddalertWalletAddress(''); // clear the form
+        // setMultWalletAryFromCookie(cookies.get('multWalletsAry')); // set array to show user on frontend
 
-            instance
-                .post(environment.backendApi + '/receiver/foxSales', {
-                    token: formAddAlertToken,
-                    enable: enable
-                })
-                .then((res) => {
-                    const data = res.data;
-
-                    // console.log(data);
-
-                    present({
-                        message: 'Successfully added the alert',
-                        color: 'success',
-                        duration: 5000
-                    });
-
-                }).catch(err => {
-
-                console.error(err);
-                setFormLoadingAddAlertToken(false); // loading false
-
+        instance
+            .post(`${environment.backendApi}/receiver/addAlertWallet`, {
+                walletAddress: formAddalertWalletAddress,
+            })
+            .then((res) => {
                 present({
-                    message: 'An error occurred when adding the Alert',
+                    message: 'Successfully added the alert',
+                    color: 'success',
+                    duration: 5000
+                });
+
+            }).catch(err => {
+                console.error(err);
+                present({
+                    message: err.msg,
                     color: 'danger',
                     duration: 5000
                 });
-            });
+            }).finally(() => setFormLoadingAddalertWalletAddress(false));
 
-            /**
-             * TO DO-alerts: alerts!
-             *
-             * in the UI tell the user the alert is going to discord over DMs
-             *
-             * once done:
-             * - test this by sending yourself tokens between wallets
-             * - add to the intro on top of fox page what this is ... then renam the cookie)
-             * - update docs.sol
-             *
-             * ONCE IN PROD:
-             * - look for enable:enable ... make it so you are able to remove the alert (unsub etc...)
-             * - add a new section to home page ...to view recent alerts?
-             */
+        /**
+         * TO DO-alerts: alerts!
+         *
+         * in the UI tell the user the alert is going to discord over DMs
+         *
+         * once done:
+         * - test this by sending yourself tokens between wallets
+         * - add to the intro on top of fox page what this is ... then renam the cookie)
+         * - update docs.sol
+         *
+         * ONCE IN PROD:
+         * - look for enable:enable ... make it so you are able to remove the alert (unsub etc...)
+         * - add a new section to home page ...to view recent alerts?
+         */
 
-        } catch (err) {
-            console.error(err);
-            setFormLoadingAddAlertToken(false); // loading false
-
-            present({
-                message: 'An error occurred when adding the Alert',
-                color: 'danger',
-                duration: 5000
-            });
-        }
     }
 
     /**
@@ -138,7 +106,7 @@ function StackedSearch({foo, onSubmit}: any) {
         <>
 
             {/*hidden={true} c*/}
-            <div hidden={true} className="secondary-bg-forced m-1 p-4 rounded-xl">
+            <div className="secondary-bg-forced m-1 p-4 rounded-xl">
                 <h4 className={`font-medium ${window.location.href.includes('fnt') ? 'text-red-600 font-medium' : ''}`}>
                     Alerts on New WL Tokens to your Wallet
                 </h4>
@@ -160,9 +128,9 @@ function StackedSearch({foo, onSubmit}: any) {
                         </IonLabel>
                         <IonInput
                             onIonChange={(e) =>
-                                setFormAddAlertToken(e.detail.value!)
+                                setFormAddalertWalletAddress(e.detail.value!)
                             }
-                            value={formAddAlertToken}
+                            value={formAddalertWalletAddress}
                             placeholder="ex. 91q2zKjAATs28sdXT5rbtKddSU81BzvJtmvZGjFj54iU"
                         ></IonInput>
                     </IonItem>
@@ -170,7 +138,7 @@ function StackedSearch({foo, onSubmit}: any) {
                     <IonButton
                         color="primary"
                         className="mt-5"
-                        hidden={formLoadingAddAlertToken}
+                        hidden={formLoadingAddalertWalletAddress}
                         onClick={() => addAlertsTokenSubmit(true)}
                     >
                         Submit
@@ -185,7 +153,7 @@ function StackedSearch({foo, onSubmit}: any) {
                     {/*    Reset Stored Wallets*/}
                     {/*</IonButton>*/}
 
-                    <div hidden={!formLoadingAddAlertToken}>Loading...</div>
+                    <div hidden={!formLoadingAddalertWalletAddress}>Loading...</div>
                 </div>
             </div>
 
@@ -202,7 +170,7 @@ function StackedSearch({foo, onSubmit}: any) {
                 The <a href="https://discord.com/channels/925207817923743794/951513272132182066" target="_blank" className="underline">#analytics-etc</a> channel in Discord
                 and the home page of the site shows when WL tokens get official names by the Famous Fox team,
                 or when a user of SOL Decoder adds a custom name to one.
-                <br/>
+                <br />
                 Visit <a href="https://discord.com/channels/925207817923743794/938996145529712651 target=_blank" className="underline">#self-roles</a> in Discord and get the <b>@fox-wl-alerts</b> role to get alerts when this happens
             </div>
 
@@ -212,7 +180,7 @@ function StackedSearch({foo, onSubmit}: any) {
                 </h4>
                 The <a href="https://discord.com/channels/925207817923743794/925215482561302529" target="_blank" className="underline">#mint-alerts-automated</a> channel in Discord
                 and the <Link to={'mintstats'} className="underline">Mint Stats</Link> page of the site is a live feed that parses links from the discords we watch. It alerts when any link could contain a new mint, before or while it is released. The mint must be linked from two discords before it shows up. On Discord, Candy Machine ID and mint details are also posted, if found.
-                <br/>
+                <br />
                 Visit <a href="https://discord.com/channels/925207817923743794/938996145529712651 target=_blank" className="underline">#self-roles</a> in Discord and get the <b>@Minter</b> role to get alerts when this happens
             </div>
 
