42f9e34bbbd9c27d2640891f7d6ca5857024341b
Dhaval DI <dhaval@disolutions.net>
2022-03-31 10:01:30 +0530

    Setup Project in IONIC

---
+++ android/app/src/main/java/com/NftDiscordRelay/MainActivity.java
@@ -0,0 +1,5 @@
+package com.NftDiscordRelay;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.NftDiscordRelay',
+  appName: 'NftDiscordRelay',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
