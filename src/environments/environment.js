16ec5aec7d936d0031b7aef5b798f8adcc352ffd
Parth Patil <parthnpatil06@gmail .com>
2022-04-03 00:02:33 +0530

    Change back client id

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -6,7 +6,7 @@ export const isDev = process.env.NODE_ENV !== 'production';
 export const environment = {
     backendApi: isDev?  'http://localhost:5001/nft-discord-relay/us-central1/api' : 'https://us-central1-nft-discord-relay.cloudfunctions.net/api',
 
-    clientId: '818767746141126656', // client ID for,
+    clientId: '927008889092857898', // client ID for,
 	ionicAppUrl : isDev ? "http://localhost:8100" : "https://soldecoder.app"
 };
 
