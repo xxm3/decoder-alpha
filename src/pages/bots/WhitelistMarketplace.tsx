cb205f5fc4e265ac094544ca8da21cac5f861d43
Mike <napindc@vt.edu>
2022-06-24 17:32:16 -0400

    top

--- src/pages/bots/WhitelistMarketplace.tsx
+++ src/pages/bots/WhitelistMarketplace.tsx
@@ -31,6 +31,24 @@ function WhitelistMarketplace() {
 
     return (
         <div>
+
+            {/*<div className="text-xl font-medium text-ellipsis flex flex-row items-center">Seamless</div>*/}
+
+            <div className="flex flex-row justify-center w-full mt-9">
+                <div className="server-module-bg p-4 px-6 w-full">
+                    <div className='w-full  items-center  mb-3'>
+                        <div className='text-xl font-semibold mb-3'>Welcome to Seamless!</div>
+
+                        This is an early look at Seamless - SOL Decoder's next joint venture with Communi3. We estimate it is only 10% complete - so plenty upgrades coming.
+                        <ul>
+                            <li>- You will have to copy/paste to join the new mint Discord manually - soon we'll have a link to help with this</li>
+                            <li>- You will have to see which DAO the whitelist is for - soon we'll have filters to help</li>
+                            <li>- Also coming soon is Twitter integration to make sure you're following, and built in giveaways to have more people join</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
             <div className="grid justify-center 2xl:grid-cols-4 xl:grid-cols-3 sm:grid-cols-2 md:gap-6 gap-4 p-10">
 
                 <div hidden={!isLoading}>
