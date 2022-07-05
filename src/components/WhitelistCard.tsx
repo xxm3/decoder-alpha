4f0c149d27521c2118bf45ec04e77128ff95c7e3
Damjan <damjandimitrov1@gmail.com>
2022-07-05 11:44:22 +0200

    Refactor whitelist raffles

--- src/components/WhitelistCard.tsx
+++ src/components/WhitelistCard.tsx
@@ -46,7 +46,9 @@ function WhitelistCard({
 	const [claiming, setClaiming] = useState<boolean>(false);
 	const queryClient = useQueryClient();
 	const [present] = useIonToast();
-	const full = type==='fcfs' ? claimCounts >= max_users : false;
+    const isFcfs = type === 'fcfs';
+    const isRaffle = type === 'raffle';
+	const full = isFcfs ? claimCounts >= max_users : false;
     const [isExploding, setIsExploding] = useState<boolean>(false);
     const uid  = localStorage.getItem('uid');
 
@@ -59,15 +61,8 @@ function WhitelistCard({
         claims:any,
         showLive: any) => {
 
-        // console.log(showLive);
-        // if(name === 'Ready Set Trade'){
-        //     console.log(expired, claiming, claimed, full , claims);
-        //     console.log(claims[0]);
-        //     console.log(uid);
-        // }
-
         if(claimed){
-            return type==='fcfs' ? 'Claimed' : 'Entered'
+            return isFcfs ? 'Claimed' : 'Entered'
         }else if(expired){
             return "Already expired"
         }else if(full){
@@ -77,12 +72,12 @@ function WhitelistCard({
         // }else if(claims[0] && claims[0].user && uid){
             // these should always match, so just returning it here. Query is on whitelistRouter.js.getQueryOptions()
             // if(claims[0].user.discordId === JSON.parse(uid)){
-            //     return type==='fcfs' ? 'Claimed' : 'Entered'
+            //     return isFcfs ? 'Claimed' : 'Entered'
             // }else{
-            //     return type==='fcfs' ? 'Claimed' : 'Entered'
+            //     return isFcfs ? 'Claimed' : 'Entered'
             // }
         }else {
-            return type==='fcfs' ? 'Obtain whitelist' : 'Enter raffle'
+            return isFcfs ? 'Obtain whitelist' : 'Enter raffle'
         }
     }
 
@@ -92,44 +87,42 @@ function WhitelistCard({
             try {
                 const {data: { won }} = await instance.post("/getRaffledState", {
                     whitelist_id : id,
-                    type: type
+                    type
                 });
 
                 if(won) {
                     setIsExploding(true);
                     present({
-                        message: 'You\'ve won whitelist raffle. You are now whitelisted in ' + sourceServer.name,
+                        message: `You've won whitelist raffle. You are now whitelisted in ${sourceServer.name}`,
                         color: 'success',
-                        duration: 10000,
+                        duration: 10_000,
                     });
                 }
             } catch {
             }
         }
 
-        if(!expired && myLiveDAO && type === 'raffle') {
+        if(!expired && myLiveDAO && isRaffle) {
             updateCardState().catch(console.error);
-        }        
+        }
     }, [expired]);
 
     // for confetti
     useEffect(() => {
-        if(claimed && type==='fcfs') {
+        if(claimed && isFcfs) {
             setIsExploding(true);
-        } else if(won && type==='raffle') {
+        } else if(won && isRaffle) {
             setIsExploding(true);
             present({
-                message: 'You\'ve won whitelist raffle. You are now whitelisted in ' + sourceServer.name,
+                message: `You've won whitelist raffle. You are now whitelisted in ${sourceServer.name}`,
                 color: 'success',
-                duration: 10000,
+                duration: 10_000,
             });
         }
     },[]);
 
     return (
-		<>
-
-        <div className="border-gray-500 border-[0.5px] rounded-2xl  overflow-clip">
+		<div className="border-gray-500 border-[0.5px] rounded-2xl  overflow-clip">
 
             {/* for confetti */}
             {isExploding && expired !== undefined && <ConfettiExplosion />}
@@ -165,7 +158,7 @@ function WhitelistCard({
                     <p>Type </p>
                     <p>{type.toUpperCase()}</p>
                     {
-                        type==='fcfs' ? 
+                        isFcfs ?
                         (<>
                             <p>Slots left </p>
                             <p>{max_users - claimCounts}/{max_users}</p>
@@ -182,15 +175,15 @@ function WhitelistCard({
                             <p>{claimCounts}</p>
                         </>)
                     }
-                    
-					<p>Required Role (in "{targetServer?.name}" DAO)</p>
-					<p>{required_role_name}</p>
-					<p className="timeLeft" hidden={expired || expired === undefined}>Time left</p>
-					<span hidden={expired || expired === undefined}><TimeAgo setExpired={setExpired} date={expiration_date}/> </span>
+
+    	<p>Required Role (in "{targetServer?.name}" DAO)</p>
+    	<p>{required_role_name}</p>
+    	<p className="timeLeft" hidden={expired || expired === undefined}>Time left</p>
+    	<span hidden={expired || expired === undefined}><TimeAgo setExpired={setExpired} date={expiration_date}/> </span>
                 </div>
 
                 {/* button! */}
-				{expired !== undefined && <IonButton css={css`
+    {expired !== undefined && <IonButton css={css`
 					--background: linear-gradient(93.86deg, #6FDDA9 0%, #6276DF 100%);
 				`} className="my-2 self-center"
 
@@ -204,35 +197,33 @@ function WhitelistCard({
                             // await instance.post("/whitelistClaims", {
                             await instance.post("/createWhitelistClaims", {
                                 whitelist_id : id,
-                                type: type
+                                type
                             });
                             queryClient.setQueryData(
                                 ['whitelistPartnerships'],
-                                (queryData) => {
-                                    return (queryData as IWhitelist[]).map(
-                                        (whitelist) => {
-                                            if (whitelist.id === id) {
-                                                whitelist.claimed = true;
-                                                whitelist.claimCounts += 1
-                                            }
-                                            return whitelist;
+                                (queryData) => (queryData as IWhitelist[]).map(
+                                    (whitelist) => {
+                                        if (whitelist.id === id) {
+                                            whitelist.claimed = true;
+                                            whitelist.claimCounts += 1
                                         }
-                                    );
-                                }
+                                        return whitelist;
+                                    }
+                                )
                             );
 
                             // success!
-                            if(type==='fcfs') {
+                            if(isFcfs) {
                                 setIsExploding(true);
                             }
 
-                            const message = type==='fcfs' ? 
-                                'Whitelist claimed successfully! You are now whitelisted in ' + sourceServer.name :
-                                'Entered whitelist raffle successfully! You are now waiting for whitelist raffle in ' + sourceServer.name;
+                            const message = isFcfs ?
+                                `Whitelist claimed successfully! You are now whitelisted in ${sourceServer.name}` :
+                                `Entered whitelist raffle successfully! You are now waiting for whitelist raffle in ${sourceServer.name}`;
                             present({
-                                message: message,
+                                message,
                                 color: 'success',
-                                duration: 10000,
+                                duration: 10_000,
                             });
 
                         // if error!
@@ -258,19 +249,17 @@ function WhitelistCard({
                         }
                     }}
 
-                     disabled={expired || claiming || claimed || full || showLive }
+                    disabled={expired || claiming || claimed || full || showLive }
                     >
                         {getButtonText(expired,claiming,claimed, full, claims, showLive)}
                     </IonButton>
 
                 }
                 {/* end button! */}
-                
-				</div>
+
+    </div>
 
 			</div>
-
-		</>
     );
 }
 
